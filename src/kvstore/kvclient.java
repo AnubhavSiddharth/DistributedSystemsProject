cee7d8fddd0fb2a171172eec1bf14b146cea53e5
AnubhavSiddharth <anubhav.echo@gmail.com>
2016-10-14 16:46:04 -0400

    Changes to Client

--- src/kvstore/kvclient.java
+++ src/kvstore/kvclient.java
@@ -8,30 +8,142 @@ import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
 
 public class kvclient {
-
- public static void main(String[] args) {
-
-  try {
-   TTransport transport;
-
-   transport = new TSocket("localhost", 9090);
-   transport.open();
-
-   TProtocol protocol = new TBinaryProtocol(transport);
-   KVStore.Client client = new KVStore.Client(protocol);
-
-   // Do something
-
-   transport.close();
-  } 
-
-  catch (TTransportException e) {
-   e.printStackTrace();
-  } 
-
-  catch (TException x) {
-   x.printStackTrace();
-  }
- }
+	
+	public static final String EMPTY_STRING = "";
+	public static final int SUCCESS = 0;
+	public static final int NOT_FOUND = 1;
+	public static final int ERROR = 2;
+	
+	/**
+	 *  Function to print generalized error.
+	 */
+	static void printError(){
+		System.out.println("Bad command!!");
+		System.exit(2);
+	}
+	
+	/**
+	 * @param result
+	 * 
+	 * Function to process result object.
+	 */
+	static void processResult(Result result){
+		String errorText = EMPTY_STRING;
+		if(null != result){
+			ErrorCode errorCode =  result.getError();
+			switch(errorCode.getValue()){
+				case SUCCESS:
+					String value = result.getValue();
+					System.out.println(value);
+					System.exit(SUCCESS);
+					break;
+					
+				case NOT_FOUND:
+					errorText = result.getErrortext();
+					if(null != errorText) {
+						System.out.println(errorText);
+					}
+					System.exit(NOT_FOUND);
+					break;
+					
+				case ERROR:
+					errorText = result.getErrortext();
+					if(null != errorText) {
+						System.out.println(errorText);
+					}
+					System.exit(ERROR);
+					break;
+			}
+		}
+		else {
+			System.out.println("Empty REsult");
+			System.exit(2);
+		}
+	}
+	
+	 /**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+	  try {
+	   TTransport transport;
+	   
+	   // Do something
+	   
+	   //Server Name
+	   String server = args[0];
+	   
+	   //Get the server address
+	   String[] address = args[1].split(":");
+	   
+	   String ip = address[0];
+	   String port = address[1];
+	   
+	   //Open a socket to server
+	   transport = new TSocket(ip, Integer.parseInt(port));
+	   transport.open();
+	
+	   TProtocol protocol = new TBinaryProtocol(transport);
+	   KVStore.Client client = new KVStore.Client(protocol);
+	   
+	   String operation = args[2];
+	   String key = EMPTY_STRING;
+	   Result res = null;	   
+	   
+	   switch(operation.substring(operation.indexOf("-") + 1, operation.length() - 1))  {
+	   
+		   case "get":
+			   if(null != args[3]){
+				   //To Do 
+				   String filename = args[5];
+				   key = args[3];
+				   res = client.kvget(key);
+				   
+				   processResult(res);
+			   }
+			   else{
+				  printError();
+			   }
+			   break;
+		   case "set":
+			   if(null != args[3] && null != args[4]){
+				   key = args[3];
+				   String value = args[4];
+				   res = client.kvset(key, value);
+				   processResult(res);
+			   }
+			   else{
+				   printError();
+			   }
+			   break;
+		   case "del":
+			   if(null != args[3]){
+				   key = args[3];
+				   res = client.kvdelete(key);
+				   processResult(res);
+			   }
+			   else{
+				   printError();
+			   }
+			   break;
+			   
+			 default:
+				 printError();
+				 break;
+	   }
+	   
+	
+	   transport.close();
+	  } 
+	
+	  catch (TTransportException e) {
+	   e.printStackTrace();
+	  } 
+	
+	  catch (TException x) {
+	   x.printStackTrace();
+	  }
+	 }
 
 }
